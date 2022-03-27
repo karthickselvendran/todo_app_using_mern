9881e9bf6e6d004b9257b5fb378c0f25a8749f73
karthickselvendran <selvendrankarthick@gmail.com>
2022-03-28 01:13:28 +0530

    codes added for routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import { TodoList } from './pages/todolist/todoList';
-import { SignIn } from './pages/signin/signin';
-import { SignUp } from './pages/signup/signup';
-import { ToastContainer } from 'react-toastify';
-import './App.css';
+// import React from 'react';
+// import { HashRouter, Routes, Route } from 'react-router-dom';
+// import { TodoList } from './pages/todolist/todoList';
+// import { SignIn } from './pages/signin/signin';
+// import { SignUp } from './pages/signup/signup';
+// import { ToastContainer } from 'react-toastify';
+// import './App.css';
 
-function App() {
+// function App() {
 
-  return (
-    <div>
-      <HashRouter basename='/'>
-        <ToastContainer />
-        <Routes>
-          <Route path='/' element={<SignIn />} />
-          <Route path='signin' element={<SignIn />} />
-          <Route path='signup' element={<SignUp />} />
-          <Route path='todolist' element={<TodoList />} />
-          <Route path='*' element={
-            <main className='wrongRoute'>
-              <p>There's nothing here, Please check the url!</p>
-            </main>
-          } />
-        </Routes>
+//   return (
+//     <div>
+//       <HashRouter basename='/'>
+//         <ToastContainer />
+//         <Routes>
+//           <Route path='/' element={<SignIn />} />
+//           <Route path='signin' element={<SignIn />} />
+//           <Route path='signup' element={<SignUp />} />
+//           <Route path='todolist' element={<TodoList />} />
+//           <Route path='*' element={
+//             <main className='wrongRoute'>
+//               <p>There's nothing here, Please check the url!</p>
+//             </main>
+//           } />
+//         </Routes>
+//       </HashRouter>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
       </HashRouter>
-    </div>
-  )
+    );
+  }
 }
+
+const Home = () => <div><h2>Home</h2></div>
+const About = () => <div><h2>About</h2></div>
 
 export default App;
